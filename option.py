7ee988e13b2d0f85295905ba5d486d3f89ef3863
popfido <wanghailin317@gmail.com>
2017-04-26 16:14:57 +0800

    Add option class

---
+++ option.py
@@ -0,0 +1,57 @@
+class Option(object):
+    """
+    Options used by embedding model.
+    """
+
+    def __init__(self, config_holder, vocab):
+        # Model options.
+
+        # Embedding dimension.
+        self.embed_dim = config_holder.embed_dim
+
+        # Window size
+        self.window_size = config_holder.window
+
+        # Number of negative samples per example.
+        self.num_samples = config_holder.num_neg
+
+        # The initial learning rate.
+        self.learning_rate = config_holder.alpha
+
+        # Number of epochs to train. After these many epochs, the learning
+        # rate decays linearly to zero and the training stops.
+        self.epochs_to_train = config_holder.epoch
+
+        # Number of negative sample
+        self.negative_sample_size = config_holder.num_neg
+
+        # Concurrent training steps.
+        # self.concurrent_steps = config_holder.concurrent_steps
+
+        # Number of examples for one training step.
+        self.batch_size = config_holder.batch_size
+
+        # The minimum number of word occurrences for it to be included in the
+        # vocabulary.
+        self.min_count = config_holder.countThreshold
+
+        # Training algorithm
+        self.train_method = config_holder.train_method
+
+        # Subsampling threshold for word occurrence.
+        self.subsample = config_holder.sample
+
+        # Vocabulary size
+        self.vocab_size = len(vocab)
+
+        # How often to print statistics.
+        self.statistics_interval = config_holder.stat_interval
+
+        # How often to write checkpoints (rounds up to the nearest statistics
+        # interval).
+        self.checkpoint_interval = config_holder.check_interval
+
+        # Which of the loss the embedding model use
+        self.loss = config_holder.loss
+
+        self.concat = config_holder.concat
